08e3635b8b81e26491e072d2811416b8bce2d8f3
anurag <annschauhan02@gmail.com>
2021-02-28 19:19:14 +0530

    Password-reset functionality added

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -126,3 +126,10 @@ LOGIN_URL = 'custom-login'
 LOGIN_REDIRECT_URL = 'blog-home'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='annschah8830@gmail.com' #os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD='1234SnnS' #os.environ.get('EMAIL_PASSWORD')
--- hello_world/urls.py
+++ hello_world/urls.py
@@ -20,6 +20,7 @@ from user import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     
     path('admin/', admin.site.urls),
@@ -32,6 +33,22 @@ urlpatterns = [
     path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='custom-login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='custom-logout'),
 
+    path('password-reset/', 
+        auth_view.PasswordResetView.as_view(template_name='password_reset.html'), 
+        name='password_reset'
+    ),
+    path('password-reset/done', 
+        auth_view.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), 
+        name='password_reset_done'
+    ),
+    path('password_reset_confirm/<uidb64>/<token>', 
+        auth_view.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), 
+        name='password_reset_confirm'
+    ),
+    path('password_reset_complete/', 
+        auth_view.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), 
+        name='password_reset_complete'
+    ),
     # path('', include('blog.urls'))
 ]
 
